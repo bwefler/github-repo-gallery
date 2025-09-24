f52a677bb1c032467961612ab54f2a01e543ee04
Bruce Wefler <bruce.wefler@gmail.com>
2025-09-24 01:27:47 -0600

    Added functions to fetch repos and display repo info

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const overview = document.querySelector(".overview");
 // My user name
 const username = "bwefler";
+// Select the unordered list to display the repos list
+const repoList = document.querySelector(".repo-list");
 
 // Async function to fetch information from GitHub profile 
 // using the GitHub API address
@@ -21,9 +23,9 @@ const fetchInfo = async function () {
     // passing the JSON data as an argument.
     displayFetchedInfo(response);
 };
-const jsonData = fetchInfo();
+fetchInfo();
 
-// Function to display the fetched user information on the page. 
+// Display the fetched user information on the page. 
 const displayFetchedInfo = function (json) {
     // (Testing access to JSON elements)
     // console.log(`Name: ${json.name}`);
@@ -32,7 +34,7 @@ const displayFetchedInfo = function (json) {
     const userInfo = document.createElement("div");
     userInfo.classList.add("user-info");
 
-    // Using innerHTML, populate the div, with elements for 
+    // Populate the div with elements for 
     // figure, image, and paragraphs
     // Use the JSON data to grab the relevant properties 
     // to display on the page.
@@ -50,8 +52,46 @@ const displayFetchedInfo = function (json) {
     // Append the div to the overview element.
     const overviewElement = document.querySelector(".overview");
     overviewElement.append(userInfo);
+
+    // Fetch repos
+    gitRepos();
 };
 
+// Async function to fetch repos
+const gitRepos = async function () {
+    // console.log("### Per page ###");
+    const fetchRepos = await 
+        fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    // console.log(fetchRepos);
 
+    // Resolve the JSON response. 
+    // console.log("### Awaiting fetchRepos.json ###");
+    const repoData = await fetchRepos.json();
+    console.log("### repoData:"); // ###
+    console.log(repoData); // ###
 
+    // Display info about each repo
+    displayRepoInfo(repoData);
+};
 
+// Display information about each repo
+const displayRepoInfo = function (repos) {
+    // Create a list item for each repo
+    for (let repo in repos) {
+        let repoListItem = document.createElement("li");
+        repoListItem.classList.add("repo");
+        
+        // Give each list item a class of "repo" and an <h3> element 
+        // with the repo name
+        let repoName = document.createElement("h3");
+        console.log(repos[repo].name);
+        repoName.innerText = repos[repo].name;
+        console.log(`### repoName ###`);
+        console.log(`${repoName.innerText}`);
+        
+        // Append the list item to the global variable that selects
+        // the unordered repos list
+        repoListItem.append(repos[repo].name);
+        repoList.append(repoListItem);
+    }
+}
